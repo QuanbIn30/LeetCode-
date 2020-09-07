9f056b39f6a243f2767e565c94247e5990e75a12
QuanbIn30 <821996940@qq.com>
2020-09-07 21:21:30 +0800

    lc-155题：最小栈

---
+++ src/lc_9_7/lc_155_MinStack.java
@@ -0,0 +1,44 @@
+package lc_9_7;
+
+import java.util.Stack;
+
+/**
+ * @author superquanbin@gmail.com
+ * @date 2020-09-07 20:45
+ */
+public class lc_155_MinStack {
+
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public lc_155_MinStack(){
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int x){
+        stack.push(x);
+        if (!minStack.isEmpty()){
+            int top = minStack.peek();
+            if(x < top)
+                minStack.push(x);
+        } else {
+           minStack.push(x);
+        }
+    }
+
+    public void pop(){
+        int pop = stack.pop();
+        int top = minStack.peek();
+        if(pop == top)
+            minStack.pop();
+    }
+
+    public int top(){
+        return stack.peek();
+    }
+
+    public int getMin(){
+        return minStack.peek();
+    }
+}
